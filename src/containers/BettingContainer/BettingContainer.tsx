5a9d9b7c2c5d8cdab3f5f0e86d44176378aa4490
mjapanwala <55627647+mjapanwala@users.noreply.github.com>
2022-09-06 14:13:23 -0400

    new edition

--- src/containers/BettingContainer/BettingContainer.tsx
+++ src/containers/BettingContainer/BettingContainer.tsx
@@ -2,10 +2,8 @@ import React, { useState, useEffect, useContext, createContext } from "react";
 import Coins from "@components/coins/coins";
 import PlaceBet from "@components/Buttons/placeBet";
 import LetsPlay from "@components/Buttons/letsPlay";
-import  {ButtonContext}  from "@containers/ButtonContainer/ButtonContainer";
-export const BetContainer = createContext({
-  
-});
+import { ButtonContext } from "@containers/ButtonContainer/ButtonContainer";
+export const BetContainer = createContext({});
 let coins = [
   {
     coinValue: 5,
@@ -35,33 +33,39 @@ let coins = [
 ];
 
 export default function BettingContainer() {
-
-  const handleButtonClick= (event, coin, index) => {
-
-    const duplicateCoins = coins.map(coin => Object.assign({}, coin));
-    const totalChips = duplicateCoins.map((item, index, array) => {
-      if (item.coinValue=== coin.coinValue && item.quantity >=1) {
-        item.quantity-= 1
-        return item;
-      } else {
-        return item
-      }
-    })
-    console.log(totalChips)
-  
-    // setCoinState (n => [...totalChips, n])
-}
   const [coinState, setCoinState] = useState(coins);
-  const [betCoins, setBetCoinState] = useState(undefined)
- 
+
+  const handleButtonClick = (event, coin, index) => {
+    const decreaseFunction = (coinStateCopy) =>
+      coinStateCopy.map((item, index, array) => {
+        if (item.coinValue === coin.coinValue && item.quantity >= 1) {
+          item.quantity -= 1;
+          return item;
+        } else {
+          return item;
+        }
+      });
+    
+    copyAndSetCoinState(decreaseFunction);
+    // setCoinState (n => [...totalChips, n])
+  };
+  const copyAndSetCoinState = (f: (coinStateCopy) => []) => {
+    // create the coinState copy
+    const coinStateCopy = coinState.map((coin) => Object.assign({}, coin));
+
+    // pass the coinstate copy to the passed in f and store its response
+    const newState = f(coinStateCopy);
+
+    // pass the response to the setCoinState function
+    setCoinState(newState);
+  };
+  const [betCoins, setBetCoinState] = useState(undefined);
+
   return (
     <>
-      <BetContainer.Provider value={{ coinState: coinState}}>
-        <Coins handleButtonClick={handleButtonClick}/>
-
-      </BetContainer.Provider> 
-
+      <BetContainer.Provider value={{ coinState: coinState }}>
+        <Coins handleButtonClick={handleButtonClick} />
+      </BetContainer.Provider>
     </>
   );
 }
-
